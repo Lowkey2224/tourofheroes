1f1da2745e88f67131e71d6a011fc6d6bf9a8af6
Marcus "Loki" Jenz <jenz@silpion.de>
2017-11-29 18:02:08 +0100

    Works completely with API. Role Integration needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-
-import { InMemoryDataService } from './in-memory-data.service';
 import { HeroService } from './hero.service';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 
@@ -34,9 +31,9 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
 // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
 // and returns simulated server responses.
 // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, {dataEncapsulation: false}
-    )
+//    HttpClientInMemoryWebApiModule.forRoot(
+//      InMemoryDataService, {dataEncapsulation: false}
+//    )
   ],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -22,10 +22,10 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
   }
 
-  add(name: string): void {
+  add(name: string, email: string, dateOfBirth: Date): void {
     name = name.trim();
     if (!name) { return; }
-    this.heroService.addHero({ name } as Hero)
+    this.heroService.addHero({ name, email, dateOfBirth } as Hero)
       .subscribe(hero => {
         this.heroes.push(hero);
       });
